a34ec7af9e8ba6912ac2ad6600a90c42d7b49807
aunetx <aunetx@yandex.com>
2019-12-22 16:26:57 +0100

    Added linear transfert function, moved doc

--- src/maths/activations.rs
+++ src/maths/activations.rs
@@ -29,14 +29,6 @@ mod utils {
     }
 }
 
-/// ### Relu transfert function :
-/// For each `x` element, returns `x` if `x > 0`, else `0`.\
-/// One of the most used activations, works with pretty everything.
-///
-/// #### Mathematically :
-/// `f(x) = max(x, 0)`
-/// #### Derivative :
-/// `∂f/∂x = 1 if x > 0, else 0`
 pub fn relu(x: Array2<f64>, derivative: bool) -> Array2<f64> {
     if !derivative {
         x.mapv(utils::max)
@@ -45,18 +37,18 @@ pub fn relu(x: Array2<f64>, derivative: bool) -> Array2<f64> {
     }
 }
 
-/// ### Sigmoid transfert function :
-/// For each `x` element, returns `1 / (1 + exp( -x ))`.\
-/// One of the most used activations, but does not fit every use case.
-///
-/// #### Mathematically :
-/// `f(x) = 1 / (1 + exp( -x ))`
-/// #### Derivative :
-/// `∂f/∂x = f(x) * (1 - f(x))`
 pub fn sigmoid(x: Array2<f64>, derivative: bool) -> Array2<f64> {
     if !derivative {
         x.mapv(|a| utils::sig(a) * (1. - utils::sig(a)))
     } else {
         x.mapv(utils::sig)
+    }
+}
+
+pub fn linear(x: Array2<f64>, derivative: bool) -> Array2<f64> {
+    if !derivative {
+        x
+    } else {
+        Array2::ones(x.raw_dim())
     }
 }
--- src/maths/mod.rs
+++ src/maths/mod.rs
@@ -4,15 +4,42 @@ use crate::Array2;
 pub type TransfertFunction = fn(Array2<f64>, bool) -> Array2<f64>;
 
 #[derive(Clone, Debug)]
+/// List the different implemented transfert function to use.
 pub enum Activation {
+    /// ### Relu transfert function :
+    /// For each `x` element, returns `x` if `x > 0`, else `0`.\
+    /// One of the most used activations, works with pretty everything.
+    ///
+    /// #### Mathematically :
+    /// `f(x) = max(x, 0)`
+    /// #### Derivative :
+    /// `∂f/∂x = 1 if x > 0, else 0`
     Relu,
+    /// ### Sigmoid transfert function :
+    /// For each `x` element, returns `1 / (1 + exp( -x ))`.\
+    /// One of the most used activations, but does not fit every use case.
+    ///
+    /// #### Mathematically :
+    /// `f(x) = 1 / (1 + exp( -x ))`
+    /// #### Derivative :
+    /// `∂f/∂x = f(x) * (1 - f(x))`
     Sigmoid,
+    /// ### Linear transfert function :
+    /// For each `x` element, returns `x`.\
+    /// Barely used, excepted for testing or simple problems.
+    ///
+    /// #### Mathematically :
+    /// `f(x) = x`
+    /// #### Derivative :
+    /// `∂f/∂x = 1`
+    Linear,
 }
 impl Activation {
     pub fn match_activation(act_type: Activation) -> TransfertFunction {
         match act_type {
             Activation::Relu => activations::relu,
             Activation::Sigmoid => activations::sigmoid,
+            Activation::Linear => activations::linear,
         }
     }
 }
